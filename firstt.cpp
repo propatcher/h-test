7a1aa8dfb2e9119b4fa550890a3359eb8960d4c5
propatcher <dfyz240206@gmail.com>
2025-03-12 13:38:49 +0400

    ttgg

--- firstt.cpp
+++ firstt.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
-using namespace std;
-
+ 
 int main()
 {
-    std::cout << "Hello";
-    return 0;
+    double a{ 1.5 }, b{}, c{}, d {-1.5};
+    double result { a / b };
+    std::cout << a << "/" << b << " = " << result << std::endl;
+    result = d / c;
+    std::cout << d << "/" << c << " = " << result << std::endl;
+    result = b / c;
+    std::cout << b << "/" << c << " = " << result << std::endl;
+    std::cout << result << " + " << a << " = " << result + a << std::endl;
 }
